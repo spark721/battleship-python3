e56ab6a7b1d6f7130c2ecfcf3134c37256abf796
spark <ses8621@gmail.com>
2019-10-05 14:30:12 -0700

    add brief overivew comment in main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 
 
 def main():
+    '''
+    initialize the game
+    loop until player's answer is no
+        start the game
+            make board
+            place ships
+            make hidden board
+            loop until game over
+                render hidden with good hits
+                player guess
+                attack
+        if game over, prompt user to play again
+    terminate
+    '''
     pass
 
 
