6acbe0dea671895047f41b0669ca91080cffa566
spark <ses8621@gmail.com>
2019-10-07 10:53:59 -0700

    add __init__, __str__, hidden functions in board class

--- board.py
+++ board.py
@@ -1,4 +1,42 @@
 
 class Board():
-    def __init__(self):
-        pass
+
+    def __init__(self, size: int = 4):
+        '''
+        init grid and size default to 4 by 4
+        run a loop to fill the grid
+        '''
+        self.size = size
+        self.grid = list()
+
+        for row in range(self.size):
+            row = list('~' * self.size)
+            self.grid.append(row)
+
+    def __str__(self):
+        '''
+        a dunder method to print the grid
+        this will get invoked if print() was used on Board instance
+        '''
+        hidden = self.hidden()
+        print('\n\tThe game board\n')
+        for i in range(self.size):
+            print('\t' + ' '.join(hidden[i]))
+        
+        return ''
+
+    def hidden(self) -> list:
+        '''
+        hide ships location
+        should only reveal a hit spot
+        return hidden_grid
+        '''
+        hidden_grid = self.grid
+        
+        for row in range(self.size):
+            for col in range(self.size):
+                pos = self.grid[row][col]
+                if pos != 'X':
+                    hidden_grid[row][col] = '~'
+        
+        return hidden_grid
