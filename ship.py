5818a052b3ae8136ae28668350b7d270caad1e98
spark <ses8621@gmail.com>
2019-10-07 15:54:18 -0700

    add random_position and valid_position method to the ship class

--- ship.py
+++ ship.py
@@ -1,10 +1,41 @@
 
 import random
+from typing import List
+
+from board import Board
 
 class Ship():
     
-    def __init__(self, size: int = 2):
+    def __init__(self, size: int = 2, vector: str = 'h'):
         '''
         set default ship size to 2
+        set vector to h for horizontal(default) or v for vertical
+        set default position to 0, 0
         '''
         self.size = size
+        self.vector = vector
+        self.pos = [0, 0]
+
+
+    def random_position(self, board: Board):
+        '''
+        generate pseudo-random numbers
+        for ship's starting position
+        '''
+        self.pos[0] = random.randrange(0, board.size)
+        self.pos[1] = random.randrange(0, board.size)
+
+
+    def valid_position(self, board: Board) -> bool:
+        '''
+        check if a ship can be place on the grid
+        '''
+        row, col = self.pos[0], self.pos[1]
+
+        if board.grid[row][col] == '~':
+            if self.vector == 'v' and row + self.size < board.size:
+                return True
+            elif self.vector == 'h' and col + self.size < board.size:
+                return True
+
+        return False
