4b0c4058c449700550bfa2fddc6f1f7b759e8fb0
spark <ses8621@gmail.com>
2019-10-08 09:47:30 -0700

    update game_over, win, lose methods in game class

--- game.py
+++ game.py
@@ -1,11 +1,21 @@
 
+from board import Board
+from player import Player
+
 class Game():
 
     def __init__(self):
-        pass
+        self.board = Board()
+        self.player = Player()
 
     def start(self):
         pass
 
-    def game_over(self) -> bool:
-        pass
+    def game_over(self, board, player) -> bool:
+        return self.win(self.board, self.player) or self.lose(self.board, self.player)
+
+    def win(self, board, player) -> bool:
+        return self.board.num_ships == 0 and self.player.guess >= 0
+
+    def lose(self, board, player) -> bool:
+        return self.board.num_ships > 0 and self.player.guess == 0
