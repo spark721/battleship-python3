f6925ac3ada2eb6928f46912e009a47d1f47fa04
spark <ses8621@gmail.com>
2019-10-08 10:25:26 -0700

    add methods to take player input for board size and attack position

--- player.py
+++ player.py
@@ -12,14 +12,31 @@ class Player():
             print('\nDefault grid size is 4')
             user_input = input('Select the grid size between 4 and 12: ')
             try:
-                user_input = int(user_input)
+                int(user_input)
             except:
                 print(f'\tInvalid input: {user_input}')
             else:
+                user_input = int(user_input)
                 if user_input >= 4 and user_input <= 12:
+                    self.guess = int(user_input * user_input / 2)
+                    return user_input
+                else: print(f'\tInvalid input: {user_input}')
+
+    def input_pos(self, size: int) -> tuple:
+        '''
+        prompt user for attack position
+        '''
+        print('\nYour turn to attack')
+        while True:
+            print('\nEnter x and y coordinates separated by space')
+            user_input = input(f'\t(between 0 and {size - 1}): ')
+            try:
+                tuple(map(lambda n: int(n), user_input.split()))
+            except:
+                print(f'\tInvalid input: {user_input}')
+            else:
+                user_input = tuple(map(lambda n: int(n), user_input.split()))
+                if user_input[0] < size and user_input[-1] < size:
                     return user_input
                 else:
-                    print(f'\tInvalid input: {user_input}')        
-
-    def input_pos(self):
-        pass
+                    print(f'\tInvalid input: {user_input}')
